83eccc005ba4ef67eedd02ede263c3bce009a02c
Daniel Macharia <danielhuios@gmail.com>
2022-08-26 13:11:56 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
-  
-}
+  const inputForm = document.querySelector("form");
+  inputForm.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-document.addEventListener('DOMContentLoaded', init);
+    const input = document.querySelector("#searchByID");
+
+    if (input.value === "" || input.value === null) {
+      alert("please fill in valid request");
+    } else {
+      fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((response) => response.json())
+        .then((data) => {
+          const title = document.querySelector("#movieDetails h4");
+          const summary = document.querySelector("#movieDetails p");
+
+          title.innerHTML = data.title;
+          summary.innerHTML = data.summary;
+        });
+    }
+  });
+};
+
+document.addEventListener("DOMContentLoaded", init);
